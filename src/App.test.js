95a38eff0db21c3140f1cfe8c2b4f7f9719e94c2
Lewis Blyth <lewisblyth@Lewiss-MacBook.lan>
2020-11-25 13:14:49 -0600

    rough image start

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import {App, add} from './App';
+
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('check if add works', () => {
+ 
+  let number  = add(3,9) // 12
+  expect(number).toStrictEqual(12);
 });
