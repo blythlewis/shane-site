840a80aa51568384883e2ac9fcc4c92e802f7d1a
benassijosef1 <jbenassi@zaizi.com>
2020-12-16 14:42:31 +0000

    block quotes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -68,22 +68,23 @@ export const Home = () => {
           </div>
         </div>
       </div>
-      <div className="block-quotes"></div>
-      {/* <div class="box">
-        <i class="fas fa-quote-left fa2"></i>
-        <div class="text">
-          <i class="fas fa-quote-right fa1"></i>
-          <div>
-            <h3>Quote the day</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+      <div className="block-quotes-wrapper">
+        <div class="box">
+          <i class="fas fa-quote-left fa2"></i>
+          <div class="text">
+            <i class="fas fa-quote-right fa1"></i>
+            <div>
+              <h3>Quote the day</h3>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </>
   );
 };
